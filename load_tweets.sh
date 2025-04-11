7b4f1187807a0c78ceb73205cdb3348882a23aac
max-baum <miba2020@mymail.pomona.edu>
2025-04-10 21:27:16 -0700

    upload

--- load_tweets.sh
+++ load_tweets.sh
@@ -7,12 +7,17 @@ files='
 test-data.zip
 '
 
-echo 'load normalized'
+#echo 'load normalized'
 for file in $files; do
+    python3 load_tweets.py --db=postgresql://postgres:pass@localhost:6768/postgres --inputs="$file"
     # call the load_tweets.py file to load data into pg_normalized
 done
 
 echo 'load denormalized'
 for file in $files; do
     # use SQL's COPY command to load data into pg_denormalized
+    
+    unzip -p "$file" > temp_raw.txt
+
+    cat temp_raw.txt | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:6767/postgres -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
